080b89722120618023c64061f61d5d47c9b88395
Asees1407 <75072372+Asees1407@users.noreply.github.com>
2022-01-22 17:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Jagdeep Singh Oberoi", "Sharandeep Sing Oberoi", "Asees Kaur Oberoi", "Harleen Kaur Oberoi"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 4
+    if(i > numbers_of_family_member_in_array )
+    {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName  = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
